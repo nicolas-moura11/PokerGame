6c64077f02d9f4cb12bbeb6d6098137f1b1f85f9
Arthur Pereira da Silva <129605534+Arthur-Pereira-da-Silva@users.noreply.github.com>
2023-09-30 19:10:49 -0300

    V0.4

--- Jogador.dart
+++ Jogador.dart
@@ -1,15 +1,67 @@
 import 'Carta.dart';
+
 class Jogador {
-  String nome;
-  List<Carta> holeCards;
-  int fichas;
+  String name;
+  int balance;
+  List<Carta> holeCards = List<Carta>.filled(2, Carta(0, ""));
+  bool isInGame;
 
-  Jogador(this.nome, this.holeCards, this.fichas);
+  Jogador({
+    required this.name,
+    required this.balance,
+    required this.holeCards,
+  }) : isInGame = true;
 
-  void mostrarHoleCards() {
-    print('$nome tem as seguintes cartas hole cards:');
-    for (var carta in holeCards) {
-      print(' - $carta');
-    }
+  String getName() {
+    return this.name;
+  }
+
+  int getBalance() {
+    return this.balance;
+  }
+
+  void addToBalance(int additionAmount) {
+    this.balance += additionAmount;
+  }
+
+  void reduceFromBalance(int reduceAmount) {
+    this.balance -= reduceAmount;
+  }
+
+  List<Carta> getHoleCards() {
+    return this.holeCards;
+  }
+
+  void setHoleCards(List<Carta> holeCards) {
+    this.holeCards = holeCards;
+  }
+
+  bool getIsInGame() {
+    return this.isInGame;
+  }
+
+  void setIsInGame(bool isInGame) {
+    this.isInGame = isInGame;
+  }
+
+  @override
+  String toString() {
+    return "Jogador: ${this.name} tem um saldo de: ${this.balance}.\nCartas: ${this.holeCards}.\nEm Jogo: ${this.isInGame}";
+  }
+
+  static void main(List<String> args) {
+    List<Carta> holeCards = [Carta(1, "Copas"), Carta(2, "Copas")];
+    Jogador p1 = Jogador(name: "Jogador1", balance: 100, holeCards: holeCards);
+    print(p1.getName());
+    print(p1.getBalance());
+    p1.addToBalance(2);
+    print(p1.getBalance());
+    p1.reduceFromBalance(2);
+    print(p1.getBalance());
+    print(p1.getHoleCards());
+    print(p1.toString());
+    List<Carta> holeCards2 = [Carta(10, "Copas"), Carta(14, "Ouros")];
+    p1.setHoleCards(holeCards2);
+    print(p1.getHoleCards());
   }
 }
